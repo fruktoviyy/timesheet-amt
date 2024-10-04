d7750eba0979731cdff10b2ddef548ec5a71609f
Smolin <msmolin@amt.ru>
2024-10-04 15:41:08 +0300

    Начальное состояние

---
+++ timesheet.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+url = "http://portal/SiteDirectory/timesheet/default.aspx?CalendarPeriod=week&CalendarDate=24%2E09%2E2024"
+
+try:
+    user_dates = input("Напишите дату начала и конца недели, за которую необходимо отправить timesheet (пример ввода: 23.09.2024-27.09.2024)")
+
+    browser = webdriver.Chrome()
+    browser.get(url)
+    time.sleep(5)
+
+    def select_days():
+        list_of_filled_days = browser.find_elements(By.CLASS_NAME, "ms-cal-tweekitem") #получаем список элементов дней с заполненными днями на выбранной неделе
+        list_of_filled_date=[] #создаем пустой список, куда будем записывать даты тех дней, которые уже были заполненны
+        if len(list_of_filled_days) != 0:
+            for i in range(len(list_of_filled_days)):
+                list_of_filled_days[i].click()
+                date_field = browser.find_element(By.CLASS_NAME, "ms-input") #ищем на страничке дату, за которую был заполненные отчет
+                list_of_filled_date.append(date_field.get_attribute("value"))
+                browser.get(url)
+                list_of_filled_days = browser.find_elements(By.CLASS_NAME, "ms-cal-tweekitem") #получаем список элементов дней с заполненными днями на выбранной неделе
+        else:
+            print("looser")
+        return list_of_filled_date
+    time.sleep(5)
+
+    
+    print(select_days())
+    
+
+
+
+
+
+except Exception as e:
+    print(f"Возникла ошибка:{e}")
